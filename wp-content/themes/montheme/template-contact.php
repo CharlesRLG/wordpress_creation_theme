9146d7ccc45ec8798d02f25b3429ec9cccaa44e2
charles <charlesrabeclegloahec@gmail.com>
2022-03-08 15:59:15 +0100

    mise en place d'un formulaire de contact, pas encore fonctionnelle

--- wp-content/themes/montheme/template-contact.php
+++ wp-content/themes/montheme/template-contact.php
@@ -10,7 +10,31 @@ get_header();
 
 
 ?>
-<p>Contact moi !!!! BORDEL !!!!!</p>
+<div id="formulaireContact">
+    <form action="#" method="POST" class="comment-form">
+        <?php wp_nonce_field('faire-don', 'cagnotte-verif'); ?>
+
+        <p>
+            <label for="don">Prénom</label>
+            <input id="don" type="text" name="don"  />
+        </p>
+        <p>
+            <label for="don">Nom</label>
+            <input id="don" type="text" name="don"  />
+        </p>
+        <p>
+            <label for="don">email</label>
+            <input id="don" type="text" name="don"  />
+        </p>
+        <p>
+            <label for="don">Description du projet</label>
+            <textarea id="don" type="text" name="don"  >
+        </p>
+        <p>
+            <input id="submit" type="submit" name="cagnote-don-envoi" id="submit" class="submit" value="Envoyer" />
+        </p>
+    </form>
+</div>
 <?php
 
 get_footer();
