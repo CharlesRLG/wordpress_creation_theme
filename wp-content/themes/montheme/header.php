34be245f733dfaeb2b9dc2bbd86f7534a954a0d2
charles <charlesrabeclegloahec@gmail.com>
2022-03-08 13:21:18 +0100

    premier commit

---
+++ wp-content/themes/montheme/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * @package Botega_Scratch_Theme
+ *
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<header>
+  
+<?php
+wp_nav_menu( array( 
+    'theme_location' => 'Mon Theme' ) ); 
+  
+?>
+</header>
+<main>
+
---
+++ wp-content/themes/montheme/template-contact.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Template Contact
+ */
+?>
+
+<?php
+
+get_header();
+
+
+?>
+<p>Contact moi !!!! BORDEL !!!!!</p>
+<?php
+
+get_footer();
